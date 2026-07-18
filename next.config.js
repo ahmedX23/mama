225db8bc154dff6d93375d2a64c7bacadcd3fc92
ahmedX23 <134196685+ahmedX23@users.noreply.github.com>
2026-07-19 02:22:27 +0600

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Next.js প্রোডাকশন বিল্ডকে আরও ফাস্ট এবং এসইও অপ্টিমাইজড করার জন্য
+  poweredByHeader: false,
+};
+
+module.exports = nextConfig;
